e6766b8f82331d2dc92be71b04e83ae2f857ab7b
LevytskyiS <levytskyi.sv@gmail.com>
2023-07-28 19:52:49 +0200

    Done
    Created an application that enables the generation of high-quality audio from a written text.

---
+++ main.py
@@ -0,0 +1,42 @@
+import json
+import time
+
+import requests
+
+from conf.config import settings
+
+
+def text_to_speech(text="Hi"):
+    headers = {"Authorization": f"Bearer {settings.API_KEY}"}
+    url = "https://api.edenai.run/v2/audio/text_to_speech"
+    payload = {
+        "providers": "lovoai",
+        "language": "en-US",
+        "option": "FEMALE",
+        "text": text,
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    result = json.loads(response.text)
+    unx_time = time.time()
+
+    with open(f"{unx_time}_chlor.json", "w", encoding="utf-8") as file:
+        json.dump(result, file, indent=4, ensure_ascii=False)
+
+    audio_url = result.get("lovoai").get("audio_resource_url")
+    r = requests.get(audio_url)
+
+    with open(f"{unx_time}.wav", "wb") as file:
+        file.write(r.content)
+
+    print("Done!")
+
+
+def main():
+    with open("text.txt", "r") as file:
+        text = file.read()
+    text_to_speech(text)
+
+
+if __name__ == "__main__":
+    main()
